bf0441c20ad69ef019a3bed21173da20a3761e9a
forthxu <forthxu@gmail.com>
2014-04-04 20:47:26 +0800

    test

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,7 @@ local max_client = 64
 skynet.start(function()
 	print("Server start")
 	local console = skynet.newservice("console")
-	skynet.newservice("debug_console",18000)
+	-- skynet.newservice("debug_console",18000)
 	skynet.newservice("simpledb")
 	local watchdog = skynet.newservice("watchdog")
 	skynet.call(watchdog, "lua", "start", {
